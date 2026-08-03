b79b5e475367d7b6e4779b62bee0aa2130fa14f5
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 16:16:02 +0530

    Add Loading component for loading state

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h2>Loading Momento...</h2>
+    </main>
+  );
+}
