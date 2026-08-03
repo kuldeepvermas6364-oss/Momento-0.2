8c944597c8bb68491b853c878abbbef87e04cd02
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 18:31:54 +0530

    Refactor Forgot Password page with Auth components

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,8 +1,26 @@
+import AuthLayout from "@/components/layout/AuthLayout";
+import AppName from "@/components/common/AppName";
+import AuthCard from "@/components/auth/AuthCard";
+import AuthHeader from "@/components/auth/AuthHeader";
+import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
+
 export default function ForgotPasswordPage() {
   return (
-    <section>
-      <h1>Forgot Password</h1>
-      <p>Password recovery page.</p>
-    </section>
+    <AuthLayout>
+      <AppName />
+
+      <div style={{ height: 24 }} />
+
+      <AuthCard>
+        <AuthHeader
+          title="Forgot Password"
+          subtitle="Reset your account password"
+        />
+
+        <div style={{ height: 24 }} />
+
+        <ForgotPasswordForm />
+      </AuthCard>
+    </AuthLayout>
   );
 }
