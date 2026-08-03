301edb1e3fdeafe0dce169df48cf9c931f2ddd16
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 16:32:39 +0530

    Add AppName component to display title

---
+++ components/common/AppName.tsx
@@ -0,0 +1,13 @@
+export default function AppName() {
+  return (
+    <h1
+      style={{
+        color: "#6366F1",
+        fontSize: "34px",
+        fontWeight: "bold",
+      }}
+    >
+      Momento
+    </h1>
+  );
+}
