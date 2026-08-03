2e0edafb415dd4caf7eccbcaa6f025ac33fd2392
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 18:52:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MainLayout from "@/components/layout/MainLayout";
 import HomeHeader from "@/components/home/HomeHeader";
 import HomeWelcome from "@/components/home/HomeWelcome";
 import CreatePostCard from "@/components/home/CreatePostCard";
-import EmptyFeed from "@/components/home/EmptyFeed";
+import HomeFeed from "@/components/home/HomeFeed";
 
 export default function HomePage() {
   return (
@@ -11,14 +11,14 @@ export default function HomePage() {
 
       <div
         style={{
-          maxWidth: "900px",
+          maxWidth: "700px",
           margin: "0 auto",
-          padding: "24px",
+          padding: "20px",
         }}
       >
         <HomeWelcome />
         <CreatePostCard />
-        <EmptyFeed />
+        <HomeFeed />
       </div>
     </MainLayout>
   );
