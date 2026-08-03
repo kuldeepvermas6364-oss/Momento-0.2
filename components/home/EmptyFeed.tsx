1b0bd0f03bfeb573622ba3eff0ca1ce130ead93d
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 18:44:20 +0530

    Add EmptyFeed component for no content display

---
+++ components/home/EmptyFeed.tsx
@@ -0,0 +1,14 @@
+export default function EmptyFeed() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "48px",
+      }}
+    >
+      <h3>No Content Yet</h3>
+
+      <p>Follow people to see their posts.</p>
+    </div>
+  );
+}
