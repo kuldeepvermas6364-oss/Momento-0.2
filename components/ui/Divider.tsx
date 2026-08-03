f8d8dea7cba5a6efe79942bf1371c63fef9051c0
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 16:12:05 +0530

    Add Divider component for horizontal separation

---
+++ components/ui/Divider.tsx
@@ -0,0 +1,11 @@
+export default function Divider() {
+  return (
+    <hr
+      style={{
+        border: "none",
+        borderTop: "1px solid #E5E7EB",
+        margin: "16px 0",
+      }}
+    />
+  );
+}
