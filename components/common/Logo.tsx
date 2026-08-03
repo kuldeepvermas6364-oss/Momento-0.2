d9f7d237e5f650b94712e15b43e50e19d3a5f2b0
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 16:10:04 +0530

    Add Logo component for branding display

---
+++ components/common/Logo.tsx
@@ -0,0 +1,13 @@
+export default function Logo() {
+  return (
+    <h1
+      style={{
+        fontSize: "32px",
+        fontWeight: "bold",
+        color: "#6366F1",
+      }}
+    >
+      Momento
+    </h1>
+  );
+}
