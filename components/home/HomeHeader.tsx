562697546633f902290177d025ad8e317f61a8bd
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 18:41:52 +0530

    Add HomeHeader component for the homepage

---
+++ components/home/HomeHeader.tsx
@@ -0,0 +1,13 @@
+export default function HomeHeader() {
+  return (
+    <header
+      style={{
+        padding: "20px",
+        background: "#FFFFFF",
+        borderBottom: "1px solid #E5E7EB"
+      }}
+    >
+      <h2>Momento</h2>
+    </header>
+  );
+}
