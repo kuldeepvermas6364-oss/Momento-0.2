a25b0c0b7e401cdcc32b959ba13dcc91ceb40ee1
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 18:44:00 +0530

    Create CreatePostCard component for user input

---
+++ components/home/CreatePostCard.tsx
@@ -0,0 +1,14 @@
+export default function CreatePostCard() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        border: "1px solid #E5E7EB",
+        borderRadius: "16px",
+        marginBottom: "20px",
+      }}
+    >
+      What's on your mind?
+    </div>
+  );
+}
