77e4c2ba4985fc082d2c8203ec85bc5c3bc45aca
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-03 16:15:41 +0530

    Add NotFound component for 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>404</h1>
+      <p>Page not found.</p>
+    </main>
+  );
+}
