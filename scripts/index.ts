d79cf53fe127a90dd2dd0c39acf027ed814c0967
kuldeepvermas6364-oss <kuldeepvermas6364@gmail.com>
2026-08-04 11:06:00 +0530

    chore: add /scripts index barrel for build and utility scripts

---
+++ scripts/index.ts
@@ -0,0 +1,14 @@
+/**
+ * /scripts/index.ts
+ * Build, deploy, and utility scripts.
+ *
+ * Scripts are run via:
+ *   npx tsx scripts/<scriptName>.ts
+ *
+ * Example scripts to be added:
+ *   scripts/seedDatabase.ts
+ *   scripts/generateTypes.ts
+ *   scripts/cleanupStorage.ts
+ */
+
+export {};
